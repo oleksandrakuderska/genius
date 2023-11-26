e7ac214dd499c7cde892e61d658cb16222182a6a
Oleksandra <oleksandrakuderska@gmail.com>
2023-11-26 15:31:22 +0100

    first commit

---
+++ js/mobile-menu.js
@@ -0,0 +1,20 @@
+// const mobilMenu = document.querySelector('.mobile-menu');
+// const menuBtnOpen = document.querySelector('.menu-btn-open');
+// const menuBtnClose = document.querySelector('.menu-btn-close');
+
+// const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+
+// menuBtnOpen.addEventListener('click', toggleMenu);
+// menuBtnClose.addEventListener('click', toggleMenu);
+
+
+
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menuBtnClose = document.querySelector('.menu-btn-close');
+
+const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleMenu);
